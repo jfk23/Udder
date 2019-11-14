bf8bbb592cc8e46a42753590517cb85def8eeecc
Byungsuk Choi <jfk23@msn.com>
2019-11-14 15:10:52 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Byungsuk Choi on 11/14/19.
+//  Copyright © 2019 Byungsuk Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
